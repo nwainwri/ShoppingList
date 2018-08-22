9ac482060ae0441695d583ded49796be2db821fb
Jamie Feeny <feeny.jamie@gmail.com>
2018-08-22 11:51:53 -0700

    Setup custom classes for MVC and started with the storyboard

---
+++ ShoppingList/View/ItemCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ItemCollectionViewCell.swift
+//  ShoppingList
+//
+//  Created by Jamie on 2018-08-22.
+//  Copyright © 2018 Jamie. All rights reserved.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+    
+}
