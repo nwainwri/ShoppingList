8404fe8e8def4034a427a83142bfe133df64e702
Jamie Feeny <feeny.jamie@gmail.com>
2018-08-24 11:57:58 -0700

    Removed Pod file and cache

--- Pods/Target Support Files/Pods-ShoppingList/Pods-ShoppingList-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ShoppingList : NSObject
-@end
-@implementation PodsDummy_Pods_ShoppingList
-@end
--- Pods/Target Support Files/Pods-ShoppingListTests/Pods-ShoppingListTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ShoppingListTests : NSObject
-@end
-@implementation PodsDummy_Pods_ShoppingListTests
-@end
