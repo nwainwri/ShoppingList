5e2f60b2171aace590425eba4185e026b64525f2
Nathan Wainwright <nathanwainwright@gmail.com>
2018-08-25 17:40:19 -0700

    sort func called when adding new, so that items are split between notdone and done

--- ShoppingList/Controller/ViewController.swift
+++ ShoppingList/Controller/ViewController.swift
@@ -142,6 +142,7 @@ class ViewController:
   {
     appData.currentItemsArray.append(sentItem)
     writeNewItem(addedItem: sentItem)
+    appData.sortEntireList()
     itemUITableView.reloadData()
   }
   
