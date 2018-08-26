29a868f4cf0cd99f2b847972f7a8036dace28981
Nathan Wainwright <nathanwainwright@gmail.com>
2018-08-25 17:21:35 -0700

    sort function almost done, needs to be sorted at first display

--- ShoppingList/Model/AppData+SortEntireList.swift
+++ ShoppingList/Model/AppData+SortEntireList.swift
@@ -17,19 +17,71 @@ extension AppData{
   // that would read the array
   // get a current index of current indexspots
   //
+  
+  // sorts by title/alphabet... need to add second condiaton to sort by bool
+  
+  
+  //ALMOST THERE
   func sortEntireList() {
-    var tempArray = [ListItem]()
-    
+//    var tempArray = [ListItem]()
+    var tempDoneItemArray = [ListItem]()
+    var tempNotDoneItemArray = [ListItem]()
+    // tempArray is entire table
+    // break into two arrays, trueArray, and false Array, sort both by title, at end of func
+    // APPEEND falseArray to true array; have that array be passed into currentArray
     for item in currentItemsArray {
       if (item.isCompleted == true) {
-        tempArray.append(item)
+        tempDoneItemArray.append(item)
+//        tempArray.append(item)
       } else {
-        tempArray.insert(item, at: 0)
+//        tempArray.insert(item, at: 0)
+        tempNotDoneItemArray.append(item)
       }
     }
-    
-    currentItemsArray = tempArray
+    tempDoneItemArray = tempDoneItemArray.sorted { $0.title < $1.title }
+    tempNotDoneItemArray = tempNotDoneItemArray.sorted { $0.title < $1.title }
+    tempNotDoneItemArray.append(contentsOf: tempDoneItemArray)
+    currentItemsArray = tempNotDoneItemArray
   }
   
   
-}
+//  //ALMOST THERE
+//  func sortEntireList() {
+//    var tempArray = [ListItem]()
+//
+//    tempArray.sorted { $0.title < $1.title }
+//
+//    for item in currentItemsArray {
+//      if (item.isCompleted == true) {
+//        print("TEST TRUE:      \(item.title)")
+//
+//        tempArray.append(item)
+//      } else {
+//        tempArray.insert(item, at: 0)
+//        print("TEST FALSE:     \(item.title)")
+//      }
+//    }
+//    currentItemsArray = tempArray
+//  }
+  
+  
+  
+  
+  
+  //  UNTOUCHED FUNC INCASE THINGS GO SIDEWAYS
+  //  func sortEntireList() {
+  //    var tempArray = [ListItem]()
+  //
+  //    for item in currentItemsArray {
+  //      if (item.isCompleted == true) {
+  //        tempArray.append(item)
+  //      } else {
+  //        tempArray.insert(item, at: 0)
+  //      }
+  //    }
+  //
+  //    currentItemsArray = tempArray
+  //  }
+  
+  
+} // END -- AppData Extension
